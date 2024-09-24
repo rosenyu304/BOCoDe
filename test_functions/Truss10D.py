448a18339a39222c5b47599288b0a5480cc93cd1
Rosen Yu <112230561+rosenyu304@users.noreply.github.com>
2024-09-24 05:39:58 -0400

    Add files via upload

---
+++ test_functions/Truss10D.py
@@ -0,0 +1,64 @@
+import torch
+import numpy as np
+from .. import TrussSolvers
+
+
+
+
+def Truss10D(individuals): 
+
+    assert torch.is_tensor(individuals) and individuals.size(1) == 10, "Input must be an n-by-10 PyTorch tensor."
+
+    n = individuals.size(0)
+    
+    fx = torch.zeros(n,1)
+
+    # 10 bar stress constraints, 4 displacement constraints
+    gx = torch.zeros(n,14)
+
+    for ii in range(n):
+        displace, stress, _, _, weights = Truss10bar(A)
+        fx[ii,0] = weights
+
+        for ss in range(10):
+            gx[ii,ss] = abs(stress[ss])
+            
+        gx[ii,10] = displace[1][1]
+        gx[ii,11] = displace[2][1]
+        gx[ii,12] = displace[4][1]
+        gx[ii,13] = displace[5][1]
+
+    return gx, fx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
