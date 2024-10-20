7985743191107bf835310eacf495b53f7a0ca2a0
kailey-m-e <kaileyme@mit.edu>
2024-10-20 11:54:06 -0400

    Update AdditionalBenchmarkProblems.py

--- Kailey/AdditionalBenchmarkProblems.py
+++ Kailey/AdditionalBenchmarkProblems.py
@@ -1,7 +1,6 @@
-import numpy as np
+import torch
 
 def happycat(X):
-
     r''' https://github.com/P-N-Suganthan/CEC2014/blob/master/Definitions%20of%20%20CEC2014%20benchmark%20suite%20Part%20A.pdf
 
     "Ye Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform for evolutionary
@@ -10,14 +9,13 @@ def happycat(X):
     # range of x = [-100, 100]
 
     X = 0.05 * X
-    fx = np.abs(np.sum(X**2, axis=1) - X.shape[1])**0.25 + \
-               (0.5 * np.sum(X**2, axis=1) + np.sum(X, axis=1)) / X.shape[1] + 0.5
+    fx = (torch.abs(torch.sum(X**2, dim=1) - X.shape[1])**0.25 +
+           (0.5 * torch.sum(X**2, dim=1) + torch.sum(X, dim=1)) / X.shape[1] + 0.5)
     gx = 0
     return gx, fx
 
 
 def discus(X):
-
     r''' https://github.com/P-N-Suganthan/CEC2014/blob/master/Definitions%20of%20%20CEC2014%20benchmark%20suite%20Part%20A.pdf
 
     "Ye Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform for evolutionary
@@ -25,13 +23,12 @@ def discus(X):
 
     # range of x = [-100, 100]
 
-    fx = 1e6 * X[:, 0]**2 + np.sum(X[:, 1:]**2, axis=1)
+    fx = 1e6 * X[:, 0]**2 + torch.sum(X[:, 1:]**2, dim=1)
     gx = 0
     return gx, fx
 
 
 def hgbat(X):
-
     r''' https://github.com/P-N-Suganthan/CEC2014/blob/master/Definitions%20of%20%20CEC2014%20benchmark%20suite%20Part%20A.pdf
 
     "Ye Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform for evolutionary
@@ -40,8 +37,8 @@ def hgbat(X):
     # range of x = [-100, 100]
 
     X = 0.05 * X - 1  # Scale and shift the input
-    fx = np.sqrt(np.abs(np.sum(X**2, axis=1)**2 - np.sum(X, axis=1)**2)) + \
-                (0.5 * np.sum(X**2, axis=1) + np.sum(X, axis=1)) / X.shape[1] + 0.5
+    fx = (torch.sqrt(torch.abs(torch.sum(X**2, dim=1)**2 - torch.sum(X, dim=1)**2)) +
+                    (0.5 * torch.sum(X**2, dim=1) + torch.sum(X, dim=1)) / X.shape[1] + 0.5)
     gx = 0
     return gx, fx
 
@@ -55,34 +52,35 @@ def schaffer(X):
     # range of x = [-100, 100]
 
     X = X**2
-    fx = np.sum(0.5 + (np.sin(np.sqrt(X + np.roll(X, -1, axis=1)))**2 - 0.5) /
-                      (1 + 0.001 * (X + np.roll(X, -1, axis=1)))**2, axis=1)
+    X_rolled = torch.roll(X, shifts=-1, dims=1)
+    fx = torch.sum(0.5 + (torch.sin(torch.sqrt(X + X_rolled))**2 - 0.5) /
+                   (1 + 0.001 * (X + X_rolled))**2, dim=1)
     gx = 0
     return gx, fx
 
-def sharp_ridge(X):
 
+def sharp_ridge(X):
     r''' https://numbbo.github.io/gforge/downloads/download16.00/bbobdocfunctions.pdf#page=70 '''
 
     # range of x = [-5, 5]
 
-    fx = X^2 + 100*np.sqrt(np.sum(X**2, axis = 1))
+    fx = X**2 + 100 * torch.sqrt(torch.sum(X**2, dim=1))
     gx = 0
     return gx, fx
 
-def different_powers(X):
 
+def different_powers(X):
     r''' https://numbbo.github.io/gforge/downloads/download16.00/bbobdocfunctions.pdf#page=60 '''
 
     # range of x = [-5, 5]
 
-    nx = len(X)
+    nx = X.shape[1]  # Number of columns
     fx = 0.0
-    z = np.abs(X)
+    z = torch.abs(X)
 
     for i in range(nx):
-        f += np.power(z[i], 2 + 4 * i / (nx - 1))
+        fx += torch.pow(z[:, i], 2 + 4 * i / (nx - 1))
 
-    fx = np.power(f, 0.5)
+    fx = torch.pow(fx, 0.5)
     gx = 0
     return gx, fx
