0b3e4027c704c362939af09e7448b4a5528acdd1
janetyq <qianjanet24@gmail.com>
2024-12-03 16:05:34 -0500

    Fix GearTrain.py

--- test_functions/GearTrain.py
+++ test_functions/GearTrain.py
@@ -18,7 +18,7 @@ from .cont_to_disc import cont_to_disc
 
 def GearTrain(X):
     # Integer Opt for Mech problems: https://asmedigitalcollection.asme.org/mechanicaldesign/article/112/2/223/417355
-    return (1/6.931 - (X[:,0]*X[:,1])/(X[:,2]*X[:,3])).reshape(-1, 1)
+    return -((1/6.931 - (X[:,0]*X[:,1])/(X[:,2]*X[:,3]))**2).reshape(-1, 1)
 
 
 
