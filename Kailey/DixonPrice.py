feea351ddff1874bc8d6f65be3897e06af95404f
kailey-m-e <kaileyme@mit.edu>
2024-12-07 22:54:30 -0500

    Update DixonPrice.py

--- Kailey/DixonPrice.py
+++ Kailey/DixonPrice.py
@@ -4,7 +4,7 @@ from base import BenchmarkProblem
 class DixonPrice(BenchmarkProblem):
 
     r'''
-
+    https://www.sfu.ca/~ssurjano/dixonpr.html
     '''
 
     # ND objective, 0 constraints, X = n-by-dim
