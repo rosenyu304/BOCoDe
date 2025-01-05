650338a74a7c11c8c50a3df0ca78b0c2ed68c621
kailey-m-e <kaileyme@mit.edu>
2025-01-05 08:53:57 -0500

    Update and rename Modact.py to MODAct.py

--- Kailey/MODAct.py
+++ Kailey/MODAct.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from base import BenchmarkProblem
 
 class MODAct(BenchmarkProblem):
@@ -9,26 +10,28 @@ class MODAct(BenchmarkProblem):
     Evolutionary Computation, pp. 1–1, 2020, doi: 10.1109/TEVC.2020.3020046.
     '''
 
-    tags = {"constrained", "continuous", "extra_imports"}
+    tags = {"constrained", "continuous", "extra_imports", "20D"}
 
     def __init__(self, problem_name):
         import modact.modact.problems as pb
         self.prob = pb.get_problem(problem_name)
         xl, xu = self.prob.bounds()
-        super().__init__(dim = 4, num_obj = len(self.prob.weights), num_cons = len(self.prob.c_weights), bounds = [[xl[i], xu[i]] for i in range(len(xl))])
+        super().__init__(dim = len(xl), num_obj = len(self.prob.weights), num_cons = len(self.prob.c_weights), bounds = [[xl[i], xu[i]] for i in range(len(xl))])
 
     def evaluate(self, X, to_verify = True):
         X = super().scale(X, to_verify)
 
-        FX = torch.zeros((X.shape[0],1))
-        GX = torch.zeros((X.shape[0],7))
+        FX = torch.zeros((X.shape[0],self.num_obj))
+        GX = torch.zeros((X.shape[0],self.num_cons))
 
         for ii in range(X.shape[0]):
             f, g = self.prob(X[ii,:].numpy())
 
             # Pymoo: f = np.array(f)*-1*cs1.weights
-            f = torch.tensor(f)*self.prob.weights # BO is maximizing
-            g = torch.tensor(g)*self.prob.c_weights
+            # print(self.prob.weights)
+            # print(self.prob.c_weights)
+            f = np.array(f)*self.prob.weights # BO is maximizing
+            g = np.array(g)*self.prob.c_weights
             FX[ii,0] = torch.tensor(f[0])
             GX[ii,:] = torch.from_numpy(g)
 
