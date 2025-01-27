05654040e18ea6e89f1b7a967d1cdd1314b853a1
kailey-m-e <kaileyme@mit.edu>
2025-01-27 14:39:08 -0500

    Create LassoBC.py

---
+++ OptBenchmarksLibrary/LassoBench/LassoBC.py
@@ -0,0 +1,38 @@
+import torch
+from ..base import *
+
+class LassoBC(BenchmarkProblem):
+
+    r'''
+    ...
+    '''
+
+    def __init__(self):
+        
+        tags = ["LassoBC",
+                "-----------------------------",
+                "OBJECTIVES: Single Objective (1)", 
+                "CONSTRAINTS: N/A", 
+                "SPACE: Continuous", 
+                "SCALABLE: 10-Dim", 
+                "IMPORTS: LassoBench",
+               ]
+        
+        super().__init__(dim=10, 
+                         num_obj = 1, 
+                         num_cons = 0, 
+                         bounds = [[-1, 1]], 
+                         tags=tags)
+
+    def _evaluate_implementation(self, X):
+
+        import LassoBench
+        fx = torch.zeros(X.shape[0],1)
+        real_bench = LassoBench.RealBenchmark(pick_data='bc')
+        for i in range(X.shape[0]):
+            # loss = real_bench.evaluate(X[i,:].numpy())
+            fx[i,0] = -real_bench.evaluate(X[i,:].to(torch.double).numpy())
+
+
+        return None, fx
+    
