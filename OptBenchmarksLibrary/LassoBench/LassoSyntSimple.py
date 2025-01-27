fd8d9ceb5c153fcfecb703d1a19b988b60622093
kailey-m-e <kaileyme@mit.edu>
2025-01-27 14:34:09 -0500

    Create LassoSyntSimple.py

---
+++ OptBenchmarksLibrary/LassoBench/LassoSyntSimple.py
@@ -0,0 +1,34 @@
+import torch
+from ..base import *
+
+class LassoSyntSimple(BenchmarkProblem):
+
+    r'''
+    ...
+    '''
+
+    def __init__(self):
+        tags = ["LassoSyntSimple",
+                "-----------------------------",
+                "OBJECTIVES: Single Objective (1)", 
+                "CONSTRAINTS: N/A", 
+                "SPACE: Continuous", 
+                "SCALABLE: 300-Dim", 
+                "IMPORTS: LassoBench",
+               ]
+        super().__init__(dim=300, 
+                         num_obj = 1, 
+                         num_cons = 0, 
+                         bounds = [[-1, 1]], 
+                         tags=tags)
+
+    def _evaluate_implementation(self, X):
+
+        import LassoBench
+        fx = torch.zeros(X.shape[0],1)
+        synt_bench = LassoBench.SyntheticBenchmark(pick_bench='synt_high')
+        for i in range(X.shape[0]):
+            fx[i,0] = -synt_bench.evaluate(X[i,:].to(torch.double).numpy())
+
+
+        return None, fx
