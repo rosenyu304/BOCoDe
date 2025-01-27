b69a6ecd06be8a51f126d3296b966578deccb726
kailey-m-e <kaileyme@mit.edu>
2025-01-27 14:31:36 -0500

    Update __init__.py

--- OptBenchmarksLibrary/LassoBench/__init__.py
+++ OptBenchmarksLibrary/LassoBench/__init__.py
@@ -1,9 +1,11 @@
 from .LassoDNA import *
 from .LassoSyntHigh import *
 from .LassoSyntMedium import *
+from .LassoSyntHard import *
 
 # Or if you want to be more explicit about what's being exported:
 __all__ = ['LassoDNA', 
            'LassoSyntHigh', 
            'LassoSyntMedium', 
+           'LassoSyntHard'
           ]
