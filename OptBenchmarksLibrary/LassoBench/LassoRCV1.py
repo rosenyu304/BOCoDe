d08d7ae85905b5d9559fe60f88e9bee47f4127aa
kailey-m-e <kaileyme@mit.edu>
2025-01-27 14:44:19 -0500

    Create LassoRCV1.py

---
+++ OptBenchmarksLibrary/LassoBench/LassoRCV1.py
@@ -0,0 +1,38 @@
+import torch
+from ..base import *
+
+class LassoRCV1(BenchmarkProblem):
+
+    r'''
+    ...
+    '''
+
+    
+
+    def __init__(self):
+        tags = ["LassoRCV1",
+                "-----------------------------",
+                "OBJECTIVES: Single Objective (1)", 
+                "CONSTRAINTS: N/A", 
+                "SPACE: Continuous", 
+                "SCALABLE: 19959-Dim", 
+                "IMPORTS: LassoBench",
+               ]
+        
+        super().__init__(dim=19959, 
+                         num_obj = 1, 
+                         num_cons = 0, 
+                         bounds = [[-1, 1]], 
+                         tags=tags)
+
+    def _evaluate_implementation(self, X):
+
+        import LassoBench
+        fx = torch.zeros(X.shape[0],1)
+        real_bench = LassoBench.RealBenchmark(pick_data='RCV1')
+        for i in range(X.shape[0]):
+            # loss = real_bench.evaluate(X[i,:].numpy())
+            fx[i,0] = -real_bench.evaluate(X[i,:].to(torch.double).numpy())
+
+
+        return None, fx
