49ee9aa8c4d42161a2dd72e367aba5802e989215
kailey-m-e <kaileyme@mit.edu>
2025-01-27 14:48:45 -0500

    Create LassoLeu.py

---
+++ OptBenchmarksLibrary/LassoBench/LassoLeu.py
@@ -0,0 +1,38 @@
+import torch
+from ..base import *
+
+class LassoLeu(BenchmarkProblem):
+
+    r'''
+    ...
+    '''
+
+    def __init__(self):
+        
+        tags = ["LassoLeu",
+                "-----------------------------",
+                "OBJECTIVES: Single Objective (1)", 
+                "CONSTRAINTS: N/A", 
+                "SPACE: Continuous", 
+                "SCALABLE: 7129-Dim", 
+                "IMPORTS: LassoBench",
+               ]
+        
+        super().__init__(dim=7129, 
+                         num_obj = 1, 
+                         num_cons = 0, 
+                         bounds = [[-1, 1]], 
+                         tags=tags)
+
+    def _evaluate_implementation(self, X):
+
+        import LassoBench
+        fx = torch.zeros(X.shape[0],1)
+        real_bench = LassoBench.RealBenchmark(pick_data='leu')
+        for i in range(X.shape[0]):
+            # loss = real_bench.evaluate(X[i,:].numpy())
+            fx[i,0] = -real_bench.evaluate(X[i,:].to(torch.double).numpy())
+
+
+        return None, fx
+    
