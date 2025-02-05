1c100138d03184ddbbef4ed8c6ddad292f30ce71
Advaith <advaith.narayanan20@gmail.com>
2025-02-04 21:59:41 -0800

    Added cont_to_disc function to Goldstein.py

--- OptBenchmarksLibrary/Synthetics/Goldstein.py
+++ OptBenchmarksLibrary/Synthetics/Goldstein.py
@@ -22,7 +22,7 @@ class Goldstein(BenchmarkProblem):
         super().__init__(dim = 2, 
                          num_obj = 1, 
                          num_cons = 0, 
-                         optimum = [-3], 
+                         optimum = [-3],
                          bounds = [[-2, 2], [0, 1]],
                          is_mixed = is_mixed,
                          debug = debug,
@@ -33,9 +33,18 @@ class Goldstein(BenchmarkProblem):
 
         # x0: [-2, 2]
         # x1: {-2, -1, 0, 1, 2}
+        def cont_to_disc(x, disc_values):
+            # Convert continuous value to discrete value
+            # Input:
+            #   x: continuous value in [0, 1]
+            #   disc_values: discrete values
+            # Output: discrete value
+            idx = torch.floor(x * len(disc_values)).long()
+            return disc_values[torch.clamp(idx, 0, len(disc_values)-1)]
+
 
         if self.is_mixed:
-            X[:,1] = super().cont_to_disc(X[:,1], torch.tensor([-2, -1, 0, 1, 2]))
+            X[:,1] = cont_to_disc(X[:,1], torch.tensor([-2, -1, 0, 1, 2]))
 
         if self.debug:
             print(f'X: {X}')
@@ -43,7 +52,7 @@ class Goldstein(BenchmarkProblem):
         fx = -((1 + (X[:,0] + X[:,1] +1)**2
                 * (19 - 14*X[:,0] + 3*X[:,0]**2 -14*X[:,1]
                     +6*X[:,0]*X[:,1] + 3*X[:,1]**2
-                    )
+                    )   
                 ) *
                 (
                     30 + (2*X[:,0] - 3*X[:,1])**2
@@ -51,5 +60,8 @@ class Goldstein(BenchmarkProblem):
                         -36*X[:,0]*X[:,1] + 27*X[:,1]**2
                     )
                 ))
+        
+        n = X.size(0)
+        fx = fx.reshape((n, 1))
 
         return None, fx
