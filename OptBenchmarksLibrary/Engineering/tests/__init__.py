596b4cdaea8ef123325e2c68fcc10aaa09c981ce
Advaith <advaith.narayanan20@gmail.com>
2025-02-19 10:30:20 -0800

    Add tests for engineering

---
+++ OptBenchmarksLibrary/Engineering/tests/__init__.py
@@ -0,0 +1,22 @@
+from .test_CarSideImpact import *
+from .test_EulerBernoulliBeamBending import *
+from .test_GearTrain import *
+from .test_RobotPush import *
+from .test_Truss10D import *
+from .test_Truss25D import *
+from .test_TwoBarTruss import *
+from .test_WaterProblem import *
+from .test_WaterResources import *
+
+# Or if you want to be more explicit about what's being exported:
+__all__ = [
+              'test_CarSideImpact',
+              'test_EulerBernoulliBeamBending',
+              'test_GearTrain',
+              'test_RobotPush',
+              'test_Truss10D',
+              'test_Truss25D',
+              'test_TwoBarTruss',
+              'test_WaterProblem',
+              'test_WaterResources',
+          ]
