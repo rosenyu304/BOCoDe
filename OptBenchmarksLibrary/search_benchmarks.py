52cf1007ca353fe69c47dfb405457e8af75435a2
Advaith Narayanan <112336645+AdvaithN1@users.noreply.github.com>
2025-03-09 02:23:16 +0000

    Optimized filtering

--- OptBenchmarksLibrary/search_benchmarks.py
+++ OptBenchmarksLibrary/search_benchmarks.py
@@ -10,6 +10,7 @@ LassoBenchFuncs = [LassoBreastCancer, LassoDiabetes, LassoDNA, LassoLeukemia, La
 EngineeringFuncs = [CarSideImpact, EulerBernoulliBeamBending, GearTrain, Mazda, Mazda_SCA, MOPTA08Car, RobotPush, Rover, Truss10D, Truss25D, TwoBarTruss, WaterProblem, WaterResources]
 CEC2020Funcs = [CEC2020_p1, CEC2020_p2, CEC2020_p3, CEC2020_p4, CEC2020_p5, CEC2020_p6, CEC2020_p7, CEC2020_p8, CEC2020_p9, CEC2020_p10, CEC2020_p11, CEC2020_p12, CEC2020_p13, CEC2020_p14, CEC2020_p15, CEC2020_p16, CEC2020_p17, CEC2020_p18, CEC2020_p19, CEC2020_p20, CEC2020_p21, CEC2020_p22, CEC2020_p23, CEC2020_p24, CEC2020_p25, CEC2020_p26, CEC2020_p27, CEC2020_p28, CEC2020_p29, CEC2020_p30, CEC2020_p31, CEC2020_p32, CEC2020_p33, CEC2020_p34, CEC2020_p35, CEC2020_p36, CEC2020_p37, CEC2020_p38, CEC2020_p39, CEC2020_p40, CEC2020_p41, CEC2020_p42, CEC2020_p43, CEC2020_p44, CEC2020_p45, CEC2020_p46, CEC2020_p47, CEC2020_p48, CEC2020_p49, CEC2020_p50, CEC2020_p51, CEC2020_p52, CEC2020_p53, CEC2020_p54, CEC2020_p55, CEC2020_p56, CEC2020_p57]
 BBOBFuncs = [BBOB, BBOB_Biobj, BBOB_BiobjMixInt, BBOB_Boxed, BBOB_Constrained, BBOB_LargeScale, BBOB_MixInt, BBOB_Noisy]
+BotorchFuncs = [BraninCurrin]
 
 categorized_classes = {
     "Synthetics": SyntheticsFuncs,
@@ -17,6 +18,7 @@ categorized_classes = {
     "Engineering": EngineeringFuncs,
     "CEC2020_RW_Constrained": CEC2020Funcs,
     "BBOB": BBOBFuncs,
+    "BoTorch": BotorchFuncs,
 }
 
 def _has_valid_val(val: ValType, constraint = Callable[[int], bool]) -> bool:
@@ -46,7 +48,7 @@ def filter_functions(dimension_filter: Callable[[int], bool] = lambda x: x > 0,
     """
     Filter functions based on the given constraints.
 
-    Available Categories: ["Synthetics", "LassoBench", "Engineering", "CEC2020_RW_Constrained", "BBOB"]
+    Available Categories: ["Synthetics", "LassoBench", "Engineering", "CEC2020_RW_Constrained", "BBOB", "BoTorch"]
 
     Parameters
     ----------
@@ -70,9 +72,8 @@ def filter_functions(dimension_filter: Callable[[int], bool] = lambda x: x > 0,
             continue
 
         for func in functions:
-            func_instance = func()
-            dimensions = getattr(func_instance, "available_dimensions", None)
-            objectives = getattr(func_instance, "num_objectives", None)
+            dimensions = getattr(func, "available_dimensions", None)
+            objectives = getattr(func, "num_objectives", None)
 
             if dimensions is not None and not _has_valid_val(dimensions, dimension_filter):
                 continue
