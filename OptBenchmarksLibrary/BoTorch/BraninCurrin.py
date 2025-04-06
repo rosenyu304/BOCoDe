1341fb86840dfc060db0b759336a5eef837db473
Advaith Narayanan <112336645+AdvaithN1@users.noreply.github.com>
2025-04-06 18:07:38 +0000

    Fixed Synthetics documentation

--- OptBenchmarksLibrary/BoTorch/BraninCurrin.py
+++ OptBenchmarksLibrary/BoTorch/BraninCurrin.py
@@ -1,41 +1,41 @@
-import torch
-from ..base import *
+# import torch
+# from ..base import *
 
 
 
-class BraninCurrin(BenchmarkProblem):
+# class BraninCurrin(BenchmarkProblem):
 
-    available_dimensions = 2
-    num_objectives = 2
+#     available_dimensions = 2
+#     num_objectives = 2
 
-    def __init__(self, 
-                 dim=2):
+#     def __init__(self, 
+#                  dim=2):
         
-        tags = ["BraninCurrin",
-                "-----------------------------",
-                "OBJECTIVES: Multi Objective (2)", 
-                "CONSTRAINTS: N/A", 
-                "SPACE: Continuous", 
-                "SCALABLE: 2-Dim", 
-                "IMPORTS: BoTorch",
-               ]
+#         tags = ["BraninCurrin",
+#                 "-----------------------------",
+#                 "OBJECTIVES: Multi Objective (2)", 
+#                 "CONSTRAINTS: N/A", 
+#                 "SPACE: Continuous", 
+#                 "SCALABLE: 2-Dim", 
+#                 "IMPORTS: BoTorch",
+#                ]
         
-        super().__init__(dim, 
-                         num_objectives = 2, 
-                         num_constraints = 0,  
-                         bounds = [[0, 1]]*2,
-                         ref_point = [18.0, 6.0],
-                         tags = tags,
-                        )
+#         super().__init__(dim, 
+#                          num_objectives = 2, 
+#                          num_constraints = 0,  
+#                          bounds = [[0, 1]]*2,
+#                          ref_point = [18.0, 6.0],
+#                          tags = tags,
+#                         )
         
 
-    def _evaluate_implementation(self, X):
-        from botorch.test_functions.multi_objective import BraninCurrin as BraninCurrin_imported
+#     def _evaluate_implementation(self, X):
+#         from botorch.test_functions.multi_objective import BraninCurrin as BraninCurrin_imported
         
-        fun = BraninCurrin_imported(negate=True)
-        fx = fun(X)
+#         fun = BraninCurrin_imported(negate=True)
+#         fx = fun(X)
 
-        return None, fx
+#         return None, fx
 
 
 
